fb597705adceeaaa5f2bd075feba0c73c648eda5
SOOYONG <wjdtndyd7@naver.com>
2023-01-11 16:25:20 +0900

    20230111

--- one-server/add_ssh_auth.sh
+++ one-server/add_ssh_auth.sh
@@ -3,6 +3,6 @@
 sshpass -p vagrant ssh -T -o strictHostKeyChecking=no vagrant@192.168.56.101
 sshpass -p vagrant ssh -T -o strictHostKeyChecking=no vagrant@192.168.56.102
 sshpass -p vagrant ssh -T -o strictHostKeyChecking=no vagrant@192.168.56.103
-sshpass -p vagrant ssh -T -o strictHostKeyChecking=no vagrant@192.168.56.104
-sshpass -p vagrant ssh -T -o strictHostKeyChecking=no vagrant@192.168.56.105
-sshpass -p vagrant ssh -T -o strictHostKeyChecking=no vagrant@192.168.56.106
+sshpass -p vagrant ssh -T -o strictHostKeyChecking=no vagrant@192.168.56.201
+sshpass -p vagrant ssh -T -o strictHostKeyChecking=no vagrant@192.168.56.202
+sshpass -p vagrant ssh -T -o strictHostKeyChecking=no vagrant@192.168.56.203
